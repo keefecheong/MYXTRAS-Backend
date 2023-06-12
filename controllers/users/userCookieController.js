d68e8bc4ffa12485b79b0373f96b679238fe338b
Wee Kang Lee <s10222162@connect.np.edu.sg>
2023-06-12 22:35:47 +0800

    login fixing progress

--- controllers/users/userCookieController.js
+++ controllers/users/userCookieController.js
@@ -5,7 +5,8 @@
 // return 204 if user in jwt is valid (invalid cookies handled in middleware)
 const verifyCookie = (req, res) => {
     if (req.user) {
-        res.status(204).end();
+        const userObject = req.user;
+        return res.status(200).json({ userObject });
     }
     else {
         res.status(401).end();
