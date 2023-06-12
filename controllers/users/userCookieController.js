0214436a1aa077978c29f42bde4921b61d6ece30
Long_Teck <longteck@gmail.com>
2023-06-12 22:46:36 +0800

    fixed bug

--- controllers/users/userCookieController.js
+++ controllers/users/userCookieController.js
@@ -5,8 +5,7 @@
 // return 204 if user in jwt is valid (invalid cookies handled in middleware)
 const verifyCookie = (req, res) => {
     if (req.user) {
-        const userObject = req.user;
-        return res.status(200).json({ userObject });
+        return res.status(200).json(req.user);
     }
     else {
         res.status(401).end();
