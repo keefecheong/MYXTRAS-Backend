e1e83b374543dec635b1c9732888874b33b437b6
Wee Kang Lee <s10222162@connect.np.edu.sg>
2023-06-14 21:27:14 +0800

    better search query

--- routes/searchRouter.js
+++ routes/searchRouter.js
@@ -4,22 +4,17 @@ const router = express.Router();
 
 // Define a route for search endpoint
 router.get('/', async (req, res) => {
+const searchTerm = req.query.term;
+const regexTerm = new RegExp(searchTerm, 'i');
 
-    const searchTerm = req.query.term;
-    // const regexTerm = new RegExp(searchTerm, 'i');
-    
-    // const topThreeForums = await Forum.find(
-    //   { $text: { $search: searchTerm } },
-    //   { score: { $meta: 'textScore' } }
-    // )
-    //   .sort({ score: { $meta: 'textScore' } })
-    //   .limit(3);
-    const topThreeForums = await Forum.find(
-        { forumID: { $regex: searchTerm, $options: 'i' } }
-      )
-        .limit(6);
-  
-    res.status(200).json({ topThreeForums }); // Return the search results as JSON
-  });
+const topThreeForums = await Forum.find({
+  $or: [
+    { forumID: { $regex: regexTerm } },
+    { forumDesc: { $regex: regexTerm } }
+  ]
+}).limit(3);
+
+res.status(200).json({ topThreeForums })
+});
   
 module.exports = router;
