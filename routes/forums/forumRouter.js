34792fec2cad3fed8e671c89b6cd9c5e63fb3aa9
Wee Kang Lee <s10222162@connect.np.edu.sg>
2023-06-14 16:16:03 +0800

    forum components update

--- routes/forums/forumRouter.js
+++ routes/forums/forumRouter.js
@@ -11,21 +11,21 @@ const forum = require('../../models/forum.js');
 // Retrieve schools
 
 router.post('/create', multerConfig.array('selectedImages'), async (req, res) => {
-    
+
+    // check if images are provided in the body
+    // if provided, continue to create post
+    // otherwise return 400 error
+
     if (!req.body) {
         res.status(400).json({ error: 'Invalid request body' });
         return;
     }
 
     try {
-        const formData = req.body;
-        const forumObjectString = formData.forumObject;
-        const forumObject = JSON.parse(forumObjectString);
-
-        // check if images are provided in the body
-        // if provided, continue to create post
-        // otherwise return 400 error
-        const { forumName, forumID, forumDesc, category } = forumObject;
+        // Check for existing forum
+        if (Forum.find({ forumID: forumID }) === null){
+            return res.status(400).json({ error: 'ForumID already exists' });
+        }
         const newForum = new Forum({
             creator_id: req.user._id,
             forumName: forumName,
@@ -42,7 +42,6 @@ router.post('/create', multerConfig.array('selectedImages'), async (req, res) =>
             await Forum.findByIdAndDelete(newForum.id);
             res.status(500).json({ message: 'Failed to upload images, please try again later.' });
         }
-        await newForum.save();
 
         const bannerUploadSuccessful = await uploadImages([req.files[1]], newForum.banner_link, newForum.id, 'forum');
         
@@ -80,25 +79,35 @@ router.post('/subscribe/:forumID', async (req, res) => {
 // Retrieve one page
 router.get('/get-forum/:forumID', async (req, res) => {
     let forum;
+    var isCreator = false;
 
     try {
         forum = await Forum.findOne({forumID : req.params.forumID});
         if (!forum) {
             return res.status(404).json({ message: 'Unable to find the specified post.' });
         }
+        
+        // Display Subscribe button in frontend logic
+        if (req.user._id.equals(forum.creator_id)){
+            isCreator = true
+        }
     } catch (error) {
         
         return res.status(500).json({ message: error.message });
     }
-    res.status(200).json(forum);
+    const response = {
+        forum: forum,
+        isCreator: isCreator,
+      };
+    res.status(200).json(response);
 });
 // Retrieve user created forum list
 router.get('/get-created-forums/', async (req, res) => {
 
     try {
-        const forums =  await Forum.find({ creator_id: req.user.id }, { forumName: 1, forum_pic_link: 1 }).exec();
+        const forums =  await Forum.find({ creator_id: req.user.id }, { forumID: 1, forumName: 1, forum_pic_link: 1 }).exec();
             // Extract the desired fields from the forums
-            const result = forums.map(({ forumName, forum_pic_link }) => ({ forumName, forum_pic_link }));
+            const result = forums.map(({ forumID, forumName, forum_pic_link }) => ({ forumID, forumName, forum_pic_link }));
 
             // Return the result as a JSON array
             res.status(200).json(result);
@@ -112,7 +121,7 @@ router.get('/get-subbed-forums/', async (req, res) => {
     let forum;
 
     try {
-        forum = await User.findById(req.params.uid).populate({ path: 'subscribed_forums', select: 'profile_pic_link forumName'})
+        forum = await User.findById(req.params.uid).populate({ path: 'subscribed_forums', select: 'forumID profile_pic_link forumName'})
         if (!forum) {
             return res.status(404).json({ message: 'Unable to find the specified post.' });
         }
