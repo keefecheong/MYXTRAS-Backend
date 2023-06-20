b03785e20a0ad62d5b1925ecaac68b581b177bb3
Wenkai Keefe Cheong <s10222423@connect.np.edu.sg>
2023-06-20 15:46:08 +0800

    update file path

--- routes/forums/threadRouter.js
+++ routes/forums/threadRouter.js
@@ -5,7 +5,7 @@ const { ObjectId } = require('mongodb');
 
 const router = express.Router();
 const { validateUserHTTP } = require('../../middleware/general/authMiddleware.js');
-const { uploadImages } = require('../../utils/posts/firebaseStorageUpload.js');
+const { uploadImages } = require('../../utils/general/firebaseStorageUpload.js');
 const { multerConfig, multerErrorHandler } = require('../../middleware/posts/multerMiddleware.js');
 
 // creating a new thread
