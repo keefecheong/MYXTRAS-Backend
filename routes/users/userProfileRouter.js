f5e8b64fdedcd7ae384eac2d39a6c1302ba9ee2b
Wee Kang Lee <s10222162@connect.np.edu.sg>
2023-06-21 23:36:24 +0800

    reg phone num and email uniqueness checker with debouncer

--- routes/users/userProfileRouter.js
+++ routes/users/userProfileRouter.js
@@ -10,10 +10,15 @@ const { multerConfig, multerErrorHandler } = require('../../middleware/posts/mul
 
 
 // get controller functions
-const { getUser, registerUser, updateUser, setupUser, getAllUsers } = require('../../controllers/users/userProfileController.js');
+const { getUser, registerUser, updateUser, setupUser, getAllUsers, verifyEmail, verifyPhoneNum} = require('../../controllers/users/userProfileController.js');
 
 // get current user from cookie
 profileRouter.get('/', validateUserHTTP, getUser);
+
+// Verify fields during registration process
+profileRouter.post('/verify-email', express.json(), verifyEmail);
+
+profileRouter.post('/verify-phone', express.json(), verifyPhoneNum);
 
 // register new user
 profileRouter.post('/', express.json(), registerUser);
@@ -26,5 +31,6 @@ profileRouter.patch('/setup', validateUserHTTP, express.json(), setupUser);
 
 // get all users except for self
 profileRouter.get('/all', validateUserHTTP, getAllUsers);
+
 
 module.exports = profileRouter;
