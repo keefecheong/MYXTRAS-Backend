9211f2ba6ccc92162bf71848a1aff0926244edaa
Long_Teck <longteck@gmail.com>
2023-07-20 01:38:42 +0800

    daily_missions

--- controllers/gamification/gamificationController.js
+++ controllers/gamification/gamificationController.js
@@ -3,19 +3,26 @@ const returnGoodReq = require('../../utils/general/returnGoodReq.js');
 const returnServerErrorReq = require('../../utils/general/returnServerErrorReq.js');
 const missions = require('../../utils/gamification/config.json');
 
+const { updateCachedUser } = require('../../cache/users/userUpdateCache.js');
+
+const saveDocAsync = require('../../utils/cache/saveDocAsync.js');
+
+
+
 async function getMissions(req, res) {
     try{
         const user = req.user;
         const daily_missions = [];
+        
         for (const task of user.daily_missions) {
             daily_missions.push({
                 'title': task,
                 'gem_count': missions.missions[task]
               });
         }
-        // console.log(user.daily_missions)
 
         returnGoodReq(res, daily_missions);
+        
 
     }
     catch (error) {
