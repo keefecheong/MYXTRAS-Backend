bd73fc8127f0716bfb0f57261b1f5219eee410cf
Yong Zi Ren <s10219524@connect.np.edu.sg>
2023-07-27 21:11:08 +0800

    update sightengine API to use testing account

--- utils/admin/moderateImage.js
+++ utils/admin/moderateImage.js
@@ -6,7 +6,7 @@ module.exports = function moderateImage(imageLink) {
     axios.get('https://api.sightengine.com/1.0/check-workflow.json', {
     params: {
         'url': imageLink,
-        'workflow': 'wfl_etq0TFMWzXigAAdbbws3m',
+        'workflow': SIGHTENGINE_WORKFLOW,
         'api_user': process.env.SIGHTENGINE_USER,
         'api_secret': process.env.SIGHTENGINE_API_KEY,
     }
