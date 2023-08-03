2d5781860d7d988040401613cbd9eaa679509278
Wenkai Keefe Cheong <s10222423@connect.np.edu.sg>
2023-08-04 00:47:15 +0800

    remove firebase initialization and packages

--- server.js
+++ server.js
@@ -32,18 +32,6 @@ require('./utils/gamification/init.js');
 // initialize cache             
 require('./cache/init.js');
 
-// // initialize Firebase (for storing files/images)
-// const { initializeApp } = require('firebase/app');
-
-// const firebaseConfig = {
-//     apiKey: process.env.FIREBASE_API_KEY,
-//     projectId: process.env.FIREBASE_PROJECT_ID,
-//     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//     appId: process.env.FIREBASE_APP_ID
-// }
-
-// initializeApp(firebaseConfig);
-
 // mount routes
 const mountRoutes = require('./routes/mountRoutes.js');
 mountRoutes(app);
