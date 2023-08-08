e3a345467250b183666846b15b1809230e28badc
Long_Teck <limlongteck04@gmail.com>
2023-08-09 00:02:55 +0800

    user model small change

--- user/models/user.js
+++ user/models/user.js
@@ -137,7 +137,7 @@ const userSchema = new mongoose.Schema({
             }
         },
         default: {
-            enabled: true,
+            enabled: false,
             inventory: [],
             chosen_pet: ''
         }
